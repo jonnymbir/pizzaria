9d5d9edc86a8c5bc8946b7f5f3c1f7bd4b34729b
John Kofi Mbir <jkmbir@gmail.com>
2022-01-15 20:26:48 +0000

    added featured page

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -3,9 +3,29 @@ import Image from "next/image";
 import styles from "../styles/Featured.module.css";
 
 function Featured() {
+
+    const images = [
+        "/img/featured.png",
+        "/img/featured2.png",
+        "/img/featured3.png",
+    ]
+
     return (
-        <div>
-           Featured 
+        <div className={styles.container}>
+            <div className={styles.arrowContainer} style={{ left: 0 }}>
+                <Image src="/img/arrowl.png" alt="" layout="fill"/> 
+           </div>
+           <div className={styles.wrapper}>
+             <div className={styles.imgContainer}>
+                {/* { images.map((img, i) => (
+                    <Image src="/img/pizza.png" alt="" key={i} layout="fill"/>
+                ))
+                } */}
+             </div>
+           </div>
+           <div className={styles.arrowContainer} style={{ left: 0 }}>
+                <Image src="/img/arrowr.png" alt="" layout="fill"/>
+           </div> 
         </div>
     )
 }

c2c0e60ff19e19dbd3d3cb35a866423f9c7e8ce0
John Kofi Mbir <jonnymbir@gmail.com>
2022-01-20 20:08:04 +0000

    pizza components updated

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -1,27 +1,23 @@
-import styles from "../styles/PizzaList.module.css"
-import PizzaCard from "./PizzaCard"
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
 
-const PizzaList = () => {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
-            <p className={styles.desc}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-            </p>
-            <div className={styles.wrapper}>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-            </div>
-        </div>
-    )
-}
+const PizzaList = ({ pizzaList }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book
+      </p>
+      <div className={styles.wrapper}>
+        {pizzaList.map((pizza) => (
+          <PizzaCard key={pizza._id} pizza={pizza}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default PizzaList
+export default PizzaList;

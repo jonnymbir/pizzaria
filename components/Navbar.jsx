6f9e455a1d9288b3e19450e723dd2bf7fe052537
John Kofi Mbir <jonnymbir@gmail.com>
2022-01-21 14:33:08 +0000

    Navbar qick update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,13 @@
 
 import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+
 function Navbar() {
+
+    const quantity = useSelector(state => state.cart.cartQuantity);
+
     return (
         <div className={styles.container}>
             <div className={styles.item}>
@@ -24,12 +30,14 @@ function Navbar() {
                     <li className={styles.listItem}>Contact</li>
                 </ul>
             </div>
+            <Link href="/cart" passHref>
             <div className={styles.item}>
                 <div className={styles.cart}>
                     <Image src="/img/cart.png" alt="" height="30px" width="30px"/>
-                    <div className={styles.counter}>2</div>
+                    <div className={styles.counter}>{quantity}</div>
                 </div>
             </div>
+            </Link>
         </div>
     )
 }
